7bd0e736237300ca3d2a0f515860a4548de02977
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-07 22:13:09 -0500

    Update job build scripts to correspond with test framework

--- jobs/FreeBSD-device-head-beaglebone-test/build.sh
+++ jobs/FreeBSD-device-head-beaglebone-test/build.sh
@@ -4,10 +4,5 @@ export TARGET=arm64
 export TARGET_ARCH=aarch64
 export DEVICE=beaglebone
 
-export USE_TEST_SUBR="
-disable-dtrace-tests.sh
-disable-zfs-tests.sh
-run-kyua.sh
-"
-
+sh -x freebsd-ci/scripts/test/extract-artifacts.sh
 #sh -x freebsd-ci/scripts/test/run-device-tests.sh
