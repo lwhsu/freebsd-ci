e934825ce928aacae168a3c6f7940f2384d8267f
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-07 22:03:49 -0500

    Fix issues with extracting/installing artifacts
    - Fix permissioning issue with 'tar'
    - Fix file writing issue for /etc/fstab
    - Enhance boot by adding /boot/loader.conf and /etc/rc.conf

--- scripts/test/extract-artifacts.sh
+++ scripts/test/extract-artifacts.sh
@@ -3,13 +3,42 @@
 DIST_PACKAGES="base kernel tests"
 DESTDIR=/b/tftpboot/${DEVICE}/install
 
+# clean up destdir
+sudo mkdir -p ${DESTDIR}
+sudo chflags -R noschg ${DESTDIR}
+sudo rm -rf ${DESTDIR}/*
+
+# install files
 for f in ${DIST_PACKAGES}
 do
-	tar Jxf ${ARTIFACTS_DIR}/${f}.txz -C ${DESTDIR}
+	sudo tar Jxf ${ARTIFACT_PATH}/${f}.txz -C ${DESTDIR}
 done
 
-cat > ${DESTDIR}/etc/fstab <<- EOF
+# Create new fstab
+cat <<- EOF | sudo tee ${DESTDIR}/etc/fstab
 	#Custom /etc/fstab for FreeBSD NFS booting
 	# Device 			Mountpoint	FSType	Options	Dump	Pass
 	10.0.0.1:${DESTDIR}		/		nfs	rw	0	0
+EOF
+
+# Create new /boot/loader.conf
+cat <<- EOF | sudo tee ${DESTDIR}/boot/loader.conf
+	# Speed up serial communications
+	boot_multicons="YES"
+	boot_serial="YES"
+	comconsole_speed="115200"
+	# Disable autoboot timer and beastie display
+	autoboot_delay="NO"
+	beastie_disable="YES"
+EOF
+
+# Create new /etc/rc.conf
+cat <<- EOF | sudo tee ${DESTDIR}/etc/rc.conf
+	# Set hostname
+	hostname=${DEVICE}-cidev
+	# Disable sendmail
+	sendmail_enable="NONE"
+	sendmail_submit_enable="NO"
+	sendmail_outbound_enable="NO"
+	sendmail_msp_queue_enable="NO"
 EOF
