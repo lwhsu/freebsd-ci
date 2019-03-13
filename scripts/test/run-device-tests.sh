c2d19ecaf6783d18beec196d0fb5125846349300
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-12 19:25:01 -0500

    Fix bugs in test device scripts
    - Syntax error in test_device
    - Extra cmdline args for expect script

--- scripts/test/run-device-tests.sh
+++ scripts/test/run-device-tests.sh
@@ -2,7 +2,6 @@
 JOB_DIR=freebsd-ci/jobs/${JOB_NAME}
 DEFAULT_TESTS_DIR=freebsd-ci/scripts/test/device_tests
 TIMEOUT=300 # 5 minute timeout
-DEVICE_PORT=cuaU0
 TESTS_DIR=${JOB_DIR}/device_tests
 cp -R ${DEFAULT_TESTS_DIR} .
 cd device_tests
@@ -10,12 +9,12 @@ if [ -d ${TESTS_DIR} ]; then
 	cp -R ${TESTS_DIR}/ .
 fi
 set +e
-./test_device.exp ${DEVICE_NAME} ${DEVICE_PORT}
+./test_device.exp
 rc=$?
 set -e
 cd -
 
 echo "return code = $rc"
-devpowerctl turn_off ${DEVICE_NAME}
+devpowerctl turn_off ${DEVICE}
 
 exit $rc
