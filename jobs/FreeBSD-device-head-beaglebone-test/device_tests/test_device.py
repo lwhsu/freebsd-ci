35d707afb213f17524f2d27e2e605c2b918af8bf
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-14 20:14:37 -0500

    Fix dummy script

--- jobs/FreeBSD-device-head-beaglebone-test/device_tests/test_device.py
+++ jobs/FreeBSD-device-head-beaglebone-test/device_tests/test_device.py
@@ -7,7 +7,7 @@ def main():
     if "DEVICE" not in environ:
         print("Invalid Usage: 'DEVICE' environment variable was not defined.", file=sys.stderr)
         sys.exit(1)
-    with open(f"{environ['DEVICE']}.boot.log") as logfile:
+    with open(f"{environ['DEVICE']}.boot.log", "w") as logfile:
         print("Skipped boot for {environ['DEVICE']}", file=logfile)
 
 if __name__ == "__main__":
