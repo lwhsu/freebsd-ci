6b5837c8f74249b6b785e19d03f621024c1a5d50
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-28 15:54:31 -0400

    Fix dtso file path

--- jobs/FreeBSD-device-d19072-beaglebone-test/overlay_dts.sh
+++ jobs/FreeBSD-device-d19072-beaglebone-test/overlay_dts.sh
@@ -2,10 +2,9 @@
 
 : ${TFTPROOT:="/b/tftpboot"}
 : ${DESTDIR:="${TFTPROOT}/${DEVICE}/install"}
-JOB_BASE=`dirname $0`
-
+JOB_DIR=freebsd-ci/jobs/${JOB_NAME}
 # Compile DTSO
-cp ${WORKSPACE}/${JOB_BASE}/am335x-boneblack-cpsw.dtso .
+cp ${JOB_DIR}/am335x-boneblack-cpsw.dtso .
 MACHINE=arm dtc -O dtb -o am335x-boneblack-cpsw.dtbo -b 0 -@ am335x-boneblack-cpsw.dtso
 sudo mkdir -p ${DESTDIR}/boot/dtbo
 sudo mv am335x-boneblack-cpsw.dtbo ${DESTDIR}/boot/dtbo
