7fa46f06e54cd54afba32e38f65d6397668b83b1
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-03-28 15:50:28 -0400

    Ensure dtso file is copied over

--- jobs/FreeBSD-device-d19072-beaglebone-test/overlay_dts.sh
+++ jobs/FreeBSD-device-d19072-beaglebone-test/overlay_dts.sh
@@ -2,8 +2,10 @@
 
 : ${TFTPROOT:="/b/tftpboot"}
 : ${DESTDIR:="${TFTPROOT}/${DEVICE}/install"}
+JOB_BASE=`dirname $0`
 
 # Compile DTSO
+cp ${WORKSPACE}/${JOB_BASE}/am335x-boneblack-cpsw.dtso .
 MACHINE=arm dtc -O dtb -o am335x-boneblack-cpsw.dtbo -b 0 -@ am335x-boneblack-cpsw.dtso
 sudo mkdir -p ${DESTDIR}/boot/dtbo
 sudo mv am335x-boneblack-cpsw.dtbo ${DESTDIR}/boot/dtbo
