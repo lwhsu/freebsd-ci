7be8569ca195272fc1d1ace428a778a2d84bd1dc
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-04-01 13:48:00 -0400

    Test 'check before clean' jail handling

--- scripts/jail/clean.sh
+++ scripts/jail/clean.sh
@@ -3,24 +3,31 @@
 . freebsd-ci/scripts/jail/jail.conf
 
 echo "clean jail ${JNAME}"
-
-sudo jexec ${JNAME} sh -c "find ${WORKSPACE_IN_JAIL} -d -not -user jenkins -flags +schg -exec chflags noschg {} \;" || true
-sudo jexec ${JNAME} sh -c "find ${WORKSPACE_IN_JAIL} -d -not -user jenkins -exec rm -rf {} \;" || true
-
-sudo jail -r ${JNAME} || true
+if [ jls -j ${JNAME} ]; then
+	sudo jexec ${JNAME} sh -c "find ${WORKSPACE_IN_JAIL} -d -not -user jenkins -flags +schg -exec chflags noschg {} \;"
+	sudo jexec ${JNAME} sh -c "find ${WORKSPACE_IN_JAIL} -d -not -user jenkins -exec rm -rf {} \;"
+	sudo jail -r ${JNAME}
+fi
 
 if [ ${BUILDER_NETIF} -a ${BUILDER_JAIL_IP6} ]; then
-	sudo ifconfig ${BUILDER_NETIF} inet6 ${BUILDER_JAIL_IP6} -alias || true
+	sudo ifconfig ${BUILDER_NETIF} inet6 ${BUILDER_JAIL_IP6} -alias
 fi
 if [ ${BUILDER_NETIF} -a ${BUILDER_JAIL_IP4} ]; then
-	sudo ifconfig ${BUILDER_NETIF} inet ${BUILDER_JAIL_IP4} -alias || true
+	sudo ifconfig ${BUILDER_NETIF} inet ${BUILDER_JAIL_IP4} -alias
 fi
 
 if [ -n "${MOUNT_REPO}" ]; then
-	sudo umount ${JPATH}/usr/${MOUNT_REPO} || true
+	PATHS=""
+	PATHS+=usr/${MOUNT_REPO#/}
+	PATHS+=dev
+	PATHS+=${WORKSPACE_IN_JAIL#/}
+	for path in PATHS; do
+		if [mount -p | grep -q ${JPATH}/${path} ]; then
+			sudo umount ${JPATH}/${path}
+		fi
+	done
 fi
-sudo umount ${JPATH}/${WORKSPACE_IN_JAIL} || true
-sudo umount ${JPATH}/dev || true
-
-sudo chflags -R noschg ${ZFS_PARENT}/${JNAME} || true
-sudo rm -rf ${ZFS_PARENT}/${JNAME} || true
+if [ -d ${ZFS_PARENT}/${JNAME} ]; then
+	sudo chflags -R noschg ${ZFS_PARENT}/${JNAME}
+	sudo rm -rf ${ZFS_PARENT}/${JNAME}
+fi
