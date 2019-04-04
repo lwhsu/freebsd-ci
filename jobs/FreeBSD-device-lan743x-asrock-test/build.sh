b42bcf41a5a176495941ed9c45abaa825870a76e
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-04-04 12:55:37 -0400

    Remove obselete jobs

--- jobs/FreeBSD-device-lan743x-asrock-test/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export TARGET=${FBSD_TARGET}
-export TARGET_ARCH=${FBSD_TARGET_ARCH}
-export DEVICE=${DEVICE_NAME}
-
-sh -x freebsd-ci/scripts/test/extract-artifacts.sh
-#sh -x freebsd-ci/scripts/test/run-device-tests.sh
