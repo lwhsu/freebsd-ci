607f5082b9c4e3df98a7255a8be3db91e263e566
Gerald Aryeetey <aryeeteygerald@rogers.com>
2019-04-09 20:56:31 -0400

    Ensure test workspace is clean before running

--- scripts/test/run-device-tests.sh
+++ scripts/test/run-device-tests.sh
@@ -3,10 +3,15 @@ JOB_DIR=freebsd-ci/jobs/${JOB_NAME}
 DEFAULT_TESTS_DIR=freebsd-ci/scripts/test/device_tests
 TIMEOUT=300 # 5 minute timeout
 TESTS_DIR=${JOB_DIR}/device_tests
+# remove current device tests
+rm -rf device_tests
+# copy in default device tests
 cp -R ${DEFAULT_TESTS_DIR} .
 if [ -d ${TESTS_DIR} ]; then
+	# copy in job specific tests
 	cp -R ${TESTS_DIR}/ device_tests/
 fi
+# run tests
 cd device_tests
 set +e
 ./test_device.py
