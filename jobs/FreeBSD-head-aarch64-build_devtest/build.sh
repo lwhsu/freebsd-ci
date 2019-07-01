9f0fb167cad8d3cf0347c52b81b3940c37e082d5
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-07-01 21:27:51 +0000

    Build kernel only and use base.txz from the official artifact server
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-aarch64-build_devtest/build.sh
+++ jobs/FreeBSD-head-aarch64-build_devtest/build.sh
@@ -1,13 +1,56 @@
 #!/bin/sh
 
-
 JOB_BASE=`dirname $0`
 cat ${JOB_BASE}/${CONF_NAME}-NFS | tee ${WORKSPACE}/src/sys/arm64/conf/${CONF_NAME}-NFS
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=${FBSD_TARGET} \
-	TARGET_ARCH=${FBSD_TARGET_ARCH} \
-	SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf \
-	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh
+JFLAG=${BUILDER_JFLAG}
+TARGET=${FBSD_TARGET}
+TARGET_ARCH=${FBSD_TARGET_ARCH}
+SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf
 
+## common script
+
+set -ex
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+
+MAKECONF=${MAKECONF:-/dev/null}
+SRCCONF=${SRCCONF:-/dev/null}
+
+cd /usr/src
+
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	kernel-toolchain \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+
+cd /usr/src/release
+
+sudo make clean
+sudo mkdir -p /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release
+sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/base.txz \
+	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
+sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
+sudo make -DNOPORTS -DNOSRC -DNODOC packagesystem \
+	TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
+	MAKE="make -DDB_FROM_SRC __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}"
+
+ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+sudo mkdir -p ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/*.txz ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/MANIFEST ${ARTIFACT_DEST}
+
+echo "r${SVN_REVISION}" | sudo tee ${ARTIFACT_DEST}/revision.txt
+
+cat <<- EOF | sudo tee ${ARTIFACT_DEST}/trigger.property
+	SVN_REVISION=${SVN_REVISION}
+EOF
