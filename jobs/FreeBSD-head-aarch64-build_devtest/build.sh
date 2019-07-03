5a398e55dc7f7867a8fe197b89d2a8adfa3dc996
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-07-03 22:45:49 +0000

    Use kernel locally
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-aarch64-build_devtest/build.sh
+++ jobs/FreeBSD-head-aarch64-build_devtest/build.sh
@@ -1,50 +1,49 @@
 #!/bin/sh
 
 JOB_BASE=`dirname $0`
-cat ${JOB_BASE}/${CONF_NAME}-NFS | tee ${WORKSPACE}/src/sys/arm64/conf/${CONF_NAME}-NFS
+#cat ${JOB_BASE}/${CONF_NAME}-NFS | tee ${WORKSPACE}/src/sys/arm64/conf/${CONF_NAME}-NFS
 
 JFLAG=${BUILDER_JFLAG}
 TARGET=${FBSD_TARGET}
 TARGET_ARCH=${FBSD_TARGET_ARCH}
-SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf
+#SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf
 
 ## common script
 
 set -ex
 
-if [ ! -n ${MOUNT_OBJ} ]; then
-	export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
-	rm -fr ${MAKEOBJDIRPREFIX}
-else
-	export MAKEOBJDIRPREFIX=/usr/${MOUNT_OBJ}
-fi
-df #XXX: debug
+#if [ ! -n ${MOUNT_OBJ} ]; then
+#	export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+#	rm -fr ${MAKEOBJDIRPREFIX}
+#else
+#	export MAKEOBJDIRPREFIX=/usr/${MOUNT_OBJ}
+#fi
 
 MAKECONF=${MAKECONF:-/dev/null}
 SRCCONF=${SRCCONF:-/dev/null}
 
 cd /usr/src
 
-#sudo make -j ${JFLAG} -DNO_CLEAN \
-#	kernel-toolchain \
-#	TARGET=${TARGET} \
-#	TARGET_ARCH=${TARGET_ARCH} \
-#	__MAKE_CONF=${MAKECONF} \
-#	SRCCONF=${SRCCONF}
-#sudo make -j ${JFLAG} -DNO_CLEAN \
-#	buildkernel \
-#	TARGET=${TARGET} \
-#	TARGET_ARCH=${TARGET_ARCH} \
-#	__MAKE_CONF=${MAKECONF} \
-#	SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	kernel-toolchain \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
 
 cd /usr/src/release
 
 sudo make clean
 sudo mkdir -p /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release
-sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/kernel.txz \
-	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
-sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
+#sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/kernel.txz \
+#	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
+#sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
 sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/base.txz \
 	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
 sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
