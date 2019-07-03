be870f38cd791a626e9e2e28ccad407eaceb8b40
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-07-03 09:53:45 +0000

    Should exit on error
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-device-head-pinea64-test/build.sh
+++ jobs/FreeBSD-device-head-pinea64-test/build.sh
@@ -4,6 +4,6 @@ export TARGET=${FBSD_TARGET}
 export TARGET_ARCH=${FBSD_TARGET_ARCH}
 export DEVICE=${DEVICE_NAME}
 
-sh -x freebsd-ci/scripts/test/extract-artifacts.sh
-sh -x freebsd-ci/scripts/test/run-device-tests.sh
+sh -ex freebsd-ci/scripts/test/extract-artifacts.sh
+sh -ex freebsd-ci/scripts/test/run-device-tests.sh
 
