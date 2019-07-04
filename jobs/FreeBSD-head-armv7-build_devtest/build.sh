71262bbcb25419ae70e36a54b48b3a028d42c3f9
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-07-04 08:35:01 +0000

    Test arm/armv7
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-armv7-build_devtest/build.sh
+++ jobs/FreeBSD-head-armv7-build_devtest/build.sh
@@ -1,13 +1,63 @@
 #!/bin/sh
 
-
 JOB_BASE=`dirname $0`
-cat ${JOB_BASE}/${CONF_NAME}-NFS | tee ${WORKSPACE}/src/sys/arm/conf/${CONF_NAME}-NFS
+#cat ${JOB_BASE}/${CONF_NAME}-NFS | tee ${WORKSPACE}/src/sys/arm/conf/${CONF_NAME}-NFS
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=${FBSD_TARGET} \
-	TARGET_ARCH=${FBSD_TARGET_ARCH} \
-	SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf \
-	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh
+JFLAG=${BUILDER_JFLAG}
+TARGET=${FBSD_TARGET}
+TARGET_ARCH=${FBSD_TARGET_ARCH}
+#SRCCONF=${WORKSPACE}/${JOB_BASE}/src.conf
 
+## common script
+
+set -ex
+
+#if [ ! -n ${MOUNT_OBJ} ]; then
+#	export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+#	rm -fr ${MAKEOBJDIRPREFIX}
+#else
+#	export MAKEOBJDIRPREFIX=/usr/${MOUNT_OBJ}
+#fi
+
+MAKECONF=${MAKECONF:-/dev/null}
+SRCCONF=${SRCCONF:-/dev/null}
+
+cd /usr/src
+
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	kernel-toolchain \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+
+cd /usr/src/release
+
+sudo make clean
+sudo mkdir -p /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release
+#sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/kernel.txz \
+#	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
+#sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/kernel.txz
+sudo fetch https://artifact.ci.freebsd.org/snapshot/head/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}/base.txz \
+	-o /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
+sudo touch /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/base.txz
+sudo make -DNOPORTS -DNOSRC -DNODOC packagesystem \
+	TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
+	MAKE="make -DDB_FROM_SRC __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}"
+
+ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+sudo mkdir -p ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/*.txz ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/MANIFEST ${ARTIFACT_DEST}
+
+echo "r${SVN_REVISION}" | sudo tee ${ARTIFACT_DEST}/revision.txt
+
+cat <<- EOF | sudo tee ${ARTIFACT_DEST}/trigger.property
+	SVN_REVISION=${SVN_REVISION}
+EOF
